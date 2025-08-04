85da7605a92e440a3dfb4523f70e9a1e1a6bdc9e
Chinnikrishna852@gmail.com <your@email.com>
2025-08-04 21:43:24 +0530

    Third Commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// vite.config.js
-export default {
+export default defineConfig({
   base: '/',
-  // ...other config
-}
+  plugins: [react()],
+})
